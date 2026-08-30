9f1bcab9bd73c84d80c4f88a7e8773c9f2af6b1b
Mason Jennings <masonj500@outlook.com>
2026-08-29 19:07:32 -0700

    add switch to fastly repo, purge_apt, vscode

--- minty-install.sh
+++ minty-install.sh
@@ -3,9 +3,20 @@
 # Configuration
 # ============================================================================
 
+SWITCH_TO_FASTLY_REPO=true
+
 INSTALL_APT_LIST_EASY=true
 INSTALL_FLATPAK_LIST_EASY=true
 PURGE_APT_LIST_EASY=true
+
+INSTALL_VSCODE=true
+#INSTALL_ESR_PURGE_FIREFOX_WITH_MODS=true
+# INSTALL_ZOOM_WITH_MODS=true
+# INSTALL_KOLOURPAINT_WITH_MODS=true
+# INSTALL_LIBREOFFICE_PURGE_APT=true
+# INSTALL_TTR=true
+# INSTALL_BOTTLES_DOWNLOAD_WIZARD=true
+
 CHANGE_GNOME_SCREENSHOT_SAVE_LOCATION=true
 GNOME_SCREENSHOT_SAVE_LOCATION=~/Documents/Screenshots
 
@@ -15,27 +26,27 @@ GNOME_SCREENSHOT_SAVE_LOCATION=~/Documents/Screenshots
 
 APT_PACKAGES_EASY=(
     git
-    chromium # install extension and set settings, if possible
-    ttf-mscore-fonts-installer
+    ttf-mscorefonts-installer
     font-manager
     neofetch
     htop
     steam-installer
+    # chromium # install extension and set settings, if possible
 )
 
 FLATPAK_PACKAGES_EASY=(
     com.github.tchx84.Flatseal
     com.tomjwatson.Emote
-    com.spotify.Client
+    org.kde.kclock
     com.belmoussaoui.Authenticator
     de.haeckerfelix.Shortwave
     com.rafaelmardojai.Blanket
-    org.kde.kclock
-    com.discordapp.Discord
     org.gnome.Aisleriot
     org.gnome.Chess
     org.localsend.localsend_app
     com.github.unrud.VideoDownloader
+    com.spotify.Client
+    com.discordapp.Discord
     # com.usebottles.bottles # also install Wizard101
 
 )
@@ -60,22 +71,51 @@ log() {
 
 greeting_function() {
     log "Welcome!"
+    sleep 1
 }
 
 exit_function() {
     log "Goodbye! Please reboot!"
+    sleep 1
+}
+
+fastly_repo() {
+    if [ "$SWITCH_TO_FASTLY_REPO" != true ] ; then
+        return 1
+    fi
+    log "Switching to Fastly CDN..."
+    for file in /etc/apt/sources.list.d/official-package-repositories.list ; do
+        sudo sed -i 's/packages.linuxmint.com/fastly.linuxmint.io/g' "$file"
+    done
+
+    update_only_apt
+}
+
+update_only_apt() {
+    log "Updating apt package list..."
+    sudo apt update
 }
 
 update_upgrade_apt() {
-    log "Updating apt packages..."
-    sudo apt update && sudo apt upgrade -y
+    update_only_apt
+    log "Upgrading apt packages..."
+    sudo apt upgrade -y
+}
+
+purge_apt_easy() {
+    if [ "$PURGE_APT_LIST_EASY" != true ] ; then
+        return 1
+    fi
+    log "purging easy apt packages"
+    sudo apt purge -y "${APT_PURGE_EASY[@]}"
+    update_only_apt
 }
 
 install_apt_easy() {
     if [ "$INSTALL_APT_LIST_EASY" != true ] ; then
         return 1
     fi
-    log "installing easy apt packages"
+    log "Installing easy apt packages..."
     sudo apt install -y "${APT_PACKAGES_EASY[@]}"
 }
 
@@ -85,22 +125,29 @@ install_flatpak_easy() {
     fi
     log "installing easy flatpak packages"
     flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-    flatpak install -y "${FLATPAK_PACKAGES_EASY}"
+    flatpak install -y "${FLATPAK_PACKAGES_EASY[@]}"
 }
-purge_apt_easy() {
-    if [ "$PURGE_APT_LIST_EASY" != true ] ; then
-        return 1
-    fi
-    log "purging easy apt packages"
-    sudo apt purge -y "${APT_PURGE_EASY[@]}"
-    sudo apt update
+
+vscode() {
+    curl -fsSL https://packages.microsoft.com/keys/microsoft.asc | sudo gpg --dearmor -o /usr/share/keyrings/microsoft.gpg
+
+    echo "Types: deb
+    URIs: https://packages.microsoft.com/repos/code
+    Suites: stable
+    Components: main
+    Architectures: amd64
+    Signed-By: /usr/share/keyrings/microsoft.gpg" |
+    sed 's/^[[:space:]]*//' |
+    sudo tee /etc/apt/sources.list.d/vscode.sources > /dev/null
 }
 
 set_screenshot_save_location() {
     if [ "${CHANGE_GNOME_SCREENSHOT_SAVE_LOCATION}" != true ] ; then
         return 1
     fi
-    log "changing save location to ${GNOME_SCREENSHOT_SAVE_LOCATION}"
+    log "changing screenshot save location to ${GNOME_SCREENSHOT_SAVE_LOCATION}"
+
+    mkdir ~/Documents/Screenshots
 }
 
 # ============================================================================
@@ -109,11 +156,15 @@ set_screenshot_save_location() {
 
 greeting_function
 sudo -v
+
+fastly_repo
 update_upgrade_apt
 
+purge_apt_easy
 install_apt_easy
 install_flatpak_easy
-purge_apt_easy
+vscode
+
 set_screenshot_save_location
 
 update_upgrade_apt
